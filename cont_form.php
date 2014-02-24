8ac399c1f25a92bbfe5809785826e4580657aa62
maisaengineering <contact@maisasolutions.com>
2014-02-24 17:25:25 +0530

    enquire form fixed

--- cont_form.php
+++ cont_form.php
@@ -1,26 +1,36 @@
 <?php
-
 require_once 'google/appengine/api/mail/Message.php';
 
 use google\appengine\api\mail\Message;
 
 // ...
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$skype = $_POST['skype'];
+$phone = $_POST['phone'];
+$company_name = $_POST['company_name'];
+$url = $_POST['url'];
+$city = $_POST['city'];
+$country = $_POST['country'];
+$work_model = $_POST['work_model'];
+$description = $_POST['description'];
 
-$message_body = " First Name  : $first_name\n\n Last Name  : $last_name\n\n Email : $email\n\n Skype : $skype\n\n Phone Number : $phone\n\n Company Name : $company_name\n\n Company URL : $url\n\n City : $city\n\n Country : $country\n\n Preferred Work Model : $work_model\n\n Description: $description";
+$message = " First Name  : $first_name\n\n Last Name  : $last_name\n\n Email : $email\n\n Skype : $skype\n\n Phone Number : $phone\n\n Company Name : $company_name\n\n Company URL : $url\n\n City : $city\n\n Country : $country\n\n Preferred Work Model : $work_model\n\n Description: $description";
 
 $mail_options = [
-    "sender" => "snk.navin143@gmail.com",
-    "to" => "naveen.maisasolutions@gmail.com",
+    "sender" => "chandan.benjaram@gmail.com",
+    "to" => "contact@maisasolutions.com",
     "subject" => "Enquire Form",
-    "textBody" => $message_body
+    "textBody" => $message
 ];
 
-try {
+	
+	try {
     $message = new Message($mail_options);
     $message->send();
     header("Location: thankyou.php");
 } catch (InvalidArgumentException $e) {
 
 }
-
 ?>

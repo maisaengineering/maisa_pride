936b4b01f4aef579db7810c6f437797ba97c00ed
maisaengineering <contact@maisasolutions.com>
2014-04-24 18:55:45 +0530

    contact page changed

--- contactus.php
+++ contactus.php
@@ -7,47 +7,47 @@
 		</section>
 	</section>
 </section>
+
 <section class="slideline"></section>
 
 <section id="main">
 	<section id="pagecontent">
 		<section id="contact1">
-				<table>
-					<tr>
-						<td align=left>
-							<span id="contactname">EMAIL US</span><br/>
-							<a href="mailto:contact@maisasolutions.com?Subject=Hello%20Maisa" class = "contact_style">contact@maisasolutions.com</a>
-						</td>							
-						<td rowspan="3" align=right>
-							<span id="contactname">OFFICE ADDRESS</span><br>
-							Flat# 201, 2nd floor<br/>
-							Mythri Apartments<br/>
-							ECIL X Roads<br/>
-							Hyderabad<br/>
-							A.P., India 500062<br/>
-						</td>
-					</tr>
-					<tr><td>&nbsp;</td></tr>
-					<tr>
-						<td>
-							<span id="contactname">CALL US</span><br/>
-							<span class = "contact_style">+1-707-709-8961 (USA)</span><br>
-							<span class = "contact_style">+91-040-401-77227 (INDIA)</span>
-						</td>
-					</tr>	
-					<tr><td>&nbsp;</td></tr>					
-					<tr>
-						<td>
-							<span id="contactname">SKYPE US</span><br/><a href="skype:cbenjaram?chat" class = "contact_style">#cbenjaram</a>
-						</td>
-					</tr>
-				</table>
+				<h2>Development Centre</h2>
+				<div>
+				Flat# 201, 2nd floor</br>
+                Mythri Apartments</br>
+                ECIL X Roads</br>
+                Hyderabad</br>
+                A.P., India 500062</br>
+                +91-040-401-77227 (INDIA)
+                </div>
 		</section>
 		<section id="contact2">
-			<section>
-				<iframe width="425" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="	http://maps.google.com/maps?source=s_q&amp;hl=en&amp;geocode=&amp;q=Maisa+Solutions,+Flat+%23+304,+Mythri+apartments,+Opp+BSNL+exchange+office,+ECIL+X+Road,&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=39.235538,79.013672&amp;vpsrc=6&amp;ie=UTF8&amp;hq=Maisa+Solutions,+Flat+%23+304,+Mythri+apartments,+Opp+BSNL+exchange+office,+ECIL+X+Road,&amp;hnear=Hyderabad,+Ranga+Reddy,+Andhra+Pradesh,+India&amp;ll=17.472267,78.568966&amp;spn=0.191891,0.181049&amp;t=m&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?source=embed&amp;hl=en&amp;geocode=&amp;q=Maisa+Solutions,+Flat+%23+304,+Mythri+apartments,+Opp+BSNL+exchange+office,+ECIL+X+Road,&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=39.235538,79.013672&amp;vpsrc=6&amp;ie=UTF8&amp;hq=Maisa+Solutions,+Flat+%23+304,+Mythri+apartments,+Opp+BSNL+exchange+office,+ECIL+X+Road,&amp;hnear=Hyderabad,+Ranga+Reddy,+Andhra+Pradesh,+India&amp;ll=17.472267,78.568966&amp;spn=0.191891,0.181049&amp;t=m" style="text-align:left" target="_blank">View</a></small>
-			</section>
+			    <h2>United States Office</h2>
+			    <div>
+			    540 Mansion Park Drive<br>
+                Suite #102<br>
+                Santa Clara, CA<br>
+                USA 95054</br>
+                +1-707-709-8961 (USA)
+                </div>
 		</section>
+	</section><div class="clear">
+	</div>
+	<section class="contact_bottom">
+		<div class="contact_email">
+			<img src="img/emailus.png">
+			<p>
+			contact@maisasolutions.com
+			</p>
+		</div>
+		<div class="contact_skyp">
+			<img src="img/skype.png">
+			<p>
+			#cbenjaram
+			</p>
+		</div>
 	</section>
 </section>
 <?php include ('footer_foot.php'); ?>

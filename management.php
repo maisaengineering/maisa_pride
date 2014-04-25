5feec2ca4338293af744bce1bd6c31c1906c87ad
maisaengineering <contact@maisasolutions.com>
2014-04-25 17:43:53 +0530

    management page changed

--- management.php
+++ management.php
@@ -3,34 +3,20 @@
 
 <section id="main">
 	<section id="pagecontent">
-		<h1>Our Pride</h1><table border="0" cellpadding="10" cellspacing="15" style="width: 100%;">
-			<table style="">
-				<tbody>
-					<tr style="padding-bottom: 9px; padding-top: 15" valign="middle">
-						<!-- <td valign="middle"><img alt="Pradeep" height="143" id="D#http://resources.css.edu/administration/images/abbotg.apter08.jpg" src="http://resources.css.edu/administration/images/abbotg.apter08.jpg" style="vertical-align: top; border: black 1px solid;" width="114" /></td> -->
-						<td>
-							<h2>Pradeep Benjaram, Founder & M.D.</h2>
-							<p class="m-desc">Pradeep, with over 15years of experience in delivering high quality output across different business sectors.</p>
-						</td>
-					</tr>
-					<tr style="padding-bottom: 9px; padding-top: 15" valign="middle">
-						<!-- <td valign="middle"><img alt="Pradeep" height="143" id="D#http://resources.css.edu/administration/images/abbotg.apter08.jpg" src="http://resources.css.edu/administration/images/abbotg.apter08.jpg" style="vertical-align: top; border: black 1px solid;" width="114" /></td> -->
-						<td>
-							<h2>Niveditha Reddy, Director of Human Resources</h2>
-							<p class="m-desc">Nivi, with over 10years of problem solving experience, plays a key role to the company for both internal & external communication. Extremely detailed and ability to bridge gap between engineering & management puts her in such a key role for us.</p>
-						</td>
-					</tr>
-					<tr style="padding-bottom: 9px; padding-top: 15" valign="middle">
-						<!-- <td valign="middle"><img alt="Pradeep" height="143" id="D#http://resources.css.edu/administration/images/abbotg.apter08.jpg" src="http://resources.css.edu/administration/images/abbotg.apter08.jpg" style="vertical-align: top; border: black 1px solid;" width="114" /></td> -->
-						<td>
-							<h2>Prasanna Patil, Director of U.S. Ops</h2>
-							<p class="m-desc"><a href="">ppatil@maisasolutions.com</a></br>Prasanna recipient of Graduate Citation holds a Masters degree from CUNY and is also PMP certified. Prasanna brings over a decade of management, business transformation and information technology consulting experience in delegating and deploying large global enterprise applications. In addition to overseeing the company’s US operations on a daily basis, Prasanna is passionately involved in client facing, ERP solution architecture and IT project management/implementations activities. Prasanna in paramount positions has consulted for several large scale global fortunes 500 clients.</p>
-						</td>
-					</tr>
+		<h2>Our Pride</h2><hr><br>
+			<article>
+				<h3>Pradeep Benjaram</h3><span>Founder & M.D.</span>
+				<p style="">Pradeep, with over 15years of experience in delivering high quality output across different business sectors.</p>
+			</article></br>
+			<article>
+				<h3>Niveditha Reddy</h3><span>Director of Human Resources</span>
+				<p>Nivi, with over 10years of problem solving experience, plays a key role to the company for both internal & external communication. Extremely detailed and ability to bridge gap between engineering & management puts her in such a key role for us.</p>
+			</article></br>
+			<article>
+				<h3>Prasanna Patil</h3><span>Director of U.S. Ops</br> <a href="">ppatil@maisasolutions.com</a></span>
 
-				</tbody>
-			</table>
-
+				<p>Prasanna recipient of Graduate Citation holds a Masters degree from CUNY and is also PMP certified. Prasanna brings over a decade of management, business transformation and information technology consulting experience in delegating and deploying large global enterprise applications. In addition to overseeing the company’s US operations on a daily basis, Prasanna is passionately involved in client facing, ERP solution architecture and IT project management/implementations activities. Prasanna in paramount positions has consulted for several large scale global fortunes 500 clients.As partner and founding member he will head the clientele management and all the implementations of Maisa’s information technology service offerings, an area in which he holds a track record of excellence.</p>
+			</article>
 		</section>
 	</section>
 <?php include ('footer_foot.php'); ?>	

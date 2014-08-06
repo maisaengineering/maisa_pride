257b9fb009c2e2de2fe9ea1eff734e7a8a8cb3c9
maisaengineering <contact@maisasolutions.com>
2014-08-06 11:07:10 +0530

    photos added in The Pride tab

--- about_us.php
+++ about_us.php
@@ -26,18 +26,28 @@
  <br>
    <h1 style="color:#C52128; font-size:22px;"><b>Our Pride</b></h1></br><hr>
         <article>
+            <div class="m_desp">
             <h1 style="color:#C52128; font-size:16px"><b>Pradeep Benjaram</b></h1><span style="color:#000000;font-size:14px;"><b>Founder & M.D.</b></span>
             <p style="">Pradeep, with over 15years of experience in delivering high quality output across different business sectors.</p>
+            </div>
+            <div class="m_img"><img src="img/pr.jpg" width="120" style="border-radius:60px"></div>
         </article>
-    </br>
+    <br>
         <article>
+           <div class="m_desp">
             <h1 style="color:#C52128; font-size:16px"><b>Niveditha Reddy</b></h1><span style="color:#000000;font-size:14px;"><b>Director of Human Resources</b></span>
             <p>Nivi, with over 10years of problem solving experience, plays a key role to the company for both internal & external communication. Extremely detailed and ability to bridge gap between engineering & management puts her in such a key role for us.</p>
+        </div>
+              <div class="m_img"><img src="img/nr.jpg" width="120" style="border-radius:60px"></div>
         </article>
-    </br>
+    <br>
         <article>
+            <div class="m_desp">
             <h1 style="color:#C52128; font-size:16px"><b>Prasanna Patil</b></h1><span style="color:#000000;font-size:14px;"><b>Director of U.S. Ops</b></span>
+            
             <p>Prasanna recipient of Graduate Citation holds a Masters degree from CUNY and is also PMP certified. Prasanna brings over a decade of management, business transformation and information technology consulting experience in delegating and deploying large global enterprise applications. In addition to overseeing the company’s US operations on a daily basis, Prasanna is passionately involved in client facing, ERP solution architecture and IT project management/implementations activities. Prasanna in paramount positions has consulted for several large scale global fortunes 500 clients.</p>
+            </div>
+            <div class="m_img"><img src="img/pp.jpg" width="120" style="border-radius:60px"></div>
         </article>
 </div>
  <hr/>
@@ -107,7 +117,7 @@
     </div>
     <div class="contact3">
         <ul class="email_cont">
-            <li><img src="img/emailus.png" width="40"><p><a href="mailto:contact@maisasolutions.com">contact@maisasolutions.com</a></p></li><br><br>
+            <li><img src="img/emailus.png" width="40"><p><a href="mailto:contact@maisasolutions.com">contact@maisasolutions.com</a></p></li>
             <li><img src="img/skype.png" width="40"><p style="margin-right: 105px;">#cbenjaram</p></li>
         </ul>
         <br>

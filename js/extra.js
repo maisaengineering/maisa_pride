90286e5859fbc2a525893ebbe65c0eb640bf6d68
imaginationcoder <labs@maisasolutions.com>
2015-02-27 11:15:32 +0530

    twitter time_line scroll

--- js/extra.js
+++ js/extra.js
@@ -71,3 +71,32 @@ window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=
        *  Open manually
        */
     });
+
+
+function parseTwitterDate(tdate) {
+    var system_date = new Date(Date.parse(tdate));
+    var user_date = new Date();
+    var post_date = tdate.split(' ');
+    var posted_date = post_date[2]  + " " +  post_date[1]
+    if (K.ie) {
+        system_date = Date.parse(tdate.replace(/( \+)/, ' UTC$1'))
+    }
+    var diff = Math.floor((user_date - system_date) / 1000);
+    if (diff <= 1) {return "just now";}
+    if (diff < 20) {return diff + " seconds ago";}
+    if (diff < 40) {return "half a minute ago";}
+    if (diff < 60) {return "less than a minute ago";}
+    if (diff <= 90) {return "one minute ago";}
+    if (diff <= 3540) {return Math.round(diff / 60) + " minutes ago";}
+    if (diff <= 5400) {return "1 hour ago";}
+    if (diff <= 86400) {return Math.round(diff / 3600) + " hours ago";}
+    return posted_date
+}
+var K = function () {
+    var a = navigator.userAgent;
+    return {
+        ie: a.match(/MSIE\s([^;]*)/)
+    }
+}();
+
+
